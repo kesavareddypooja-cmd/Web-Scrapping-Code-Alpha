c87e70b9687277913f326b9c0056ba401c9815c1
kesavareddypooja-cmd <kesavareddypooja@gmail.com>
2026-07-27 21:16:54 +0530

    Create machine.py

---
+++ machine.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://quotes.toscrape.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = []
+authors = []
+
+for quote in soup.find_all("span", class_="text"):
+    quotes.append(quote.text)
+
+for author in soup.find_all("small", class_="author"):
+    authors.append(author.text)
+
+df = pd.DataFrame({
+    "Quote": quotes,
+    "Author": authors
+})
+
+print(df.head())
+
+df.to_csv("quotes.csv", index=False)
